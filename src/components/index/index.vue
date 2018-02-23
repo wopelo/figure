7d02b274d2afc2a1c0b5466ea7833a74a2d1435f
wopelo <815001837@qq.com>
2018-02-23 14:15:40 +0800

    完成预览删除

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,13 +1,15 @@
 <template>
 	<div>
 		<div id="message">
-			已选择{{number}}张，总大小{{size}}KB
+			已选择{{number}}张，总大小{{size}}/{{max}}KB
 		</div>
 
 		<div id="upload">
 			<div class="uploadOption" v-on:click="upload()">
-				<span class="icon-upload"></span>
-				<span>上传</span>
+				<label>
+					<span class="icon-upload"></span>
+					<span>上传</span>
+				</label>
 			</div>
 			<div class="uploadOption">
 				<label for="fileInput">
@@ -18,14 +20,33 @@
 		</div>
 
 		<div id="uploadControl">
-			<input type="file" multiple="multiple" accept="image/*" id="fileInput" v-on:change="fileChnage($event)">
+			<input type="file" multiple="multiple" accept="image/*" id="fileInput" v-on:change="fileChange($event)">
 		</div>
 
 		<div id="uploadCanvas">
 			<div class="uploadItem" v-for="item in imageArray">
-				<div class="itemPreview" v-bind:style="{backgroundImage:'url('+item+')'}">
-					<span class="icon-remove remove"></span>
+				<div class="itemPreview" v-bind:style="{backgroundImage:'url('+item.dataURL+')'}">
+					<span class="icon-remove remove" v-on:click="remove(item.id)"></span>
 				</div>
+			</div>
+		</div>
+
+		<div id="curtain" v-show="isError">
+			
+		</div>
+
+		<div id="waring" v-show="isError">
+			<div id="waringTitle">
+				<span class="icon-remove" v-on:click="isError=false"></span>
+			</div>
+			<div id="waringContent">
+				<p>
+					哦豁，出错了
+				</p>
+				<p>
+					{{waring}}
+				</p>
+				<button v-on:click="isError=false" id="iKnow">知道啦</button>
 			</div>
 		</div>
 	</div>
@@ -37,41 +58,69 @@
 			return {
 				number:0,
 				size:0,
+				max:10240,
 				imageArray:[],
-				upmpImage:new FormData(),
-				isClick:false
+				isClick:false,
+				waring:"警告信息",
+				isError:false,
+				fileInput:""
 			}
 		},
 		methods:{
 			upload(){
 				console.log(this.imageArray);
 			},
-			fileChnage(event){
+			fileChange(event){
 				let file=event.target.files;
+				this.fileInput=event.target;
 				for(let n=0;n<file.length;n++){
 					console.log(file[n]);
 					//防止用户强行选择其他文件
 					if(file[n].type.split("/")[0]!="image"){
-						alert("请选择图片类型文件");
+						this.isError=true;
+						this.waring="请选择图片类型文件";
 						break;
 					}else{
 						//以key:value形式，将图片存到FormData中
 						this.number++;
-						this.size+=((file[n].size/1024).toFixed(2)-0);
-						this.upmpImage.append(file[n].name.split(".")[0],file[n]);
+						let itemSize=parseInt((file[n].size/1024));
+						this.size+=itemSize;
+						//设置最大上传大小
+						if(this.size>parseInt(this.max)){
+							this.isError=true;
+							this.waring="超过容量上限";
+							this.size-=itemSize;
+							break;
+						}
 						//生成预览
 						this.preview(file[n]);
 					}
 				}
+				this.clear();
 			},
 			preview(file){
 				let reader=new FileReader();
+				let id=file.name.split(".")[0];
 				reader.onloadend=()=>{
 				    let dataURL=reader.result;
-					this.imageArray.push(dataURL);
+					this.imageArray.push({"dataURL":dataURL,"id":id,"size":parseInt((file.size/1024))});
 					console.log(this.imageArray);
 				};
 				reader.readAsDataURL(file); // 读出 base64
+			},
+			remove(id){
+				console.log(id);
+				for(let n=0;n<this.imageArray.length;n++){
+					if(this.imageArray[n].id==id){
+						this.size-=this.imageArray[n].size;
+						this.imageArray.splice(n,1);
+					}
+				}
+				this.clear();
+			},
+			//允许重复选择相同的文件
+			clear(){
+				this.fileInput.value="";
 			}
 		}
 	}
