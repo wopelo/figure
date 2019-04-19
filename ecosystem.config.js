5a832ce9b5082b98e0dcf3c6557d1fe37fdacb25
江承远 <815001837@qq.com>
2019-04-19 17:38:16 +0800

    feat: 尝试使用pm2部署

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  apps : [{
+    name: 'figure_pm2',
+    script: 'prod.server.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '123.207.156.15',
+      key: '../ssh',
+      ssh_options: 'StrictHostKeyChecking=no',
+      ref  : 'origin/master',
+      repo : 'git@github.com:wopelo/figure.git',
+      path : '~/node_work/figure_pm2',
+      'post-deploy' : 'git pull && pm2 startOrRestart ./ecosystem.config.js --env production'
+    }
+  }
+};
